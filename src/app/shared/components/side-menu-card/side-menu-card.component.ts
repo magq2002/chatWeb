81236090af9b74173f66672352a6f9e9ecff8b20
AsusMiguel <migalvez2002@gmail.com>
2024-05-21 21:16:37 -0500

    "Updated names and messages in SideMenuCardComponent's data array."

--- src/app/shared/components/side-menu-card/side-menu-card.component.ts
+++ src/app/shared/components/side-menu-card/side-menu-card.component.ts
@@ -15,12 +15,13 @@ export class SideMenuCardComponent {
     {id: 1, name: "Sport ChatBot", message:"Hola!, pregúntame algo...", date: "Hoy", number: 1, avatar: this.sportBot},
     {id: 2, name: "Miguel Galvez", message:"Vamos a ir el sábado", date: "Jueves", number: 2, avatar: this.crypoBot},
     {id: 3, name: "Maryuri", message:"El otro viernes", date: "Martes", number: 1, avatar: this.crypoBot},
-    {id: 4, name: "Valentina", message:"Si vio eso", date: "Miercoles", number: 4, avatar: this.crypoBot},
+    {id: 4, name: "Cami", message:"Qué mas?", date: "Miercoles", number: 4, avatar: this.crypoBot},
     {id: 5, name: "Jhon", message:"Ahorita miro", date: "Domingo", number: 2, avatar: this.crypoBot},
     {id: 6, name: "Billy", message:"Mañana hago eso", date: "Jueves", number: 7, avatar: this.crypoBot},
-    {id: 7, name: "Angular", message:"Signals al poder", date: "Jueves", number: 2, avatar: this.crypoBot},
-    {id: 8, name: "Vue", message:"Svelte mi hijo", date: "Sábado", number: 3, avatar: this.crypoBot},
-    {id: 9, name: "Java", message:"No se de java", date: "Viernes", number: 5, avatar: this.crypoBot},
+    {id: 7, name: "Valentina", message:"Si vio eso", date: "Viernes", number: 5, avatar: this.crypoBot},
+    {id: 8, name: "Angular", message:"Signals al poder", date: "Jueves", number: 2, avatar: this.crypoBot},
+    {id: 9, name: "Vue", message:"Svelte mi hijo", date: "Sábado", number: 3, avatar: this.crypoBot},
+    {id: 10, name: "Java", message:"No se de java", date: "Viernes", number: 5, avatar: this.crypoBot},
   ]
 
 }
